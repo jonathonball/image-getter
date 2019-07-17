bb09e30b6fcc7afa091b53b89a6e667bf0a8898d
Jon Ball <jonathon.ball@gmail.com>
2019-07-17 07:27:50 -0400

    well, adding signal handling sure was easy

--- main.c
+++ main.c
@@ -3,25 +3,35 @@
  * Inspirations:
  * - http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
  * - https://curl.haxx.se/libcurl/c/url2file.html
+ * - http://www.yolinux.com/TUTORIALS/C++Signals.html
  */
 #define NUM_ENV_KEYS 8
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include <signal.h>
 #include "speak.h"
 #include "environment.h"
 #include "daemonize.h"
 #include "curl.h"
 
+int PROCESS_CONTINUE = 1;
+
+void shutdown_signal(int signum) {
+    PROCESS_CONTINUE = 0;
+}
+
 int main(int argc, char **argv, char **environ) {
+    signal(SIGQUIT, shutdown_signal);
+    signal(SIGINT, shutdown_signal);
     speak("Attempting initialization");
     gather_environment_details();
     validate_environment_details();
     discuss_environment();
     daemonize();
     int interval = atoi(get_environment_value("TIMEOUT"));
-    while (1) {
+    while (PROCESS_CONTINUE) {
         get_image();
         sleep(interval);
     }
